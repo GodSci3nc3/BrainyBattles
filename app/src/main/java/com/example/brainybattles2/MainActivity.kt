792cec8f1ac434148bfdf7bdd3c4c0bd61b39cfa
clemezap <clemezap@gmail.com>
2024-06-11 20:31:04 -0600

    Limpieza de código de preguntas extra

--- app/src/main/java/com/example/brainybattles2/MainActivity.kt
+++ app/src/main/java/com/example/brainybattles2/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.example.brainybattles2
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.Window
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.example.brainy_bat.Domain.QuestionModel
+import com.example.brainybattles2.Activity.QuizActivity
+import com.example.brainybattles2.Activity.splashQuiz
 
 import com.example.brainybattles2.databinding.ActivityMainBinding
+import com.example.brainybattles2.databinding.SplashQuizBinding
 import com.ismaeldivita.chipnavigation.ChipNavigationBar
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -21,6 +34,7 @@ import org.json.JSONObject
 class MainActivity : MainClass() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bindingPrueba: SplashQuizBinding
     private lateinit var menu: ChipNavigationBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,7 +52,7 @@ class MainActivity : MainClass() {
 
 
         lifecycleScope.launch(Dispatchers.IO){
-            //queue()
+
             getUser().collect(){
                 Log.d("DataStore", "Store picture: ${it.picture}")
                 withContext(Dispatchers.Main){
@@ -77,8 +91,6 @@ class MainActivity : MainClass() {
         }
 
 
-
-
         val window: Window = this@MainActivity.window
         window.statusBarColor = ContextCompat.getColor(this@MainActivity, R.color.grey)
 
@@ -100,7 +112,6 @@ class MainActivity : MainClass() {
 
         }
 
-
         binding.apply {
 
             menu.setItemSelected(R.id.Home)
@@ -110,51 +121,145 @@ class MainActivity : MainClass() {
             singleBtn.setOnClickListener {
 
                 // AQUÍ SE INICIARIA LA CARGA DE PREGUNTAS
-
-                val i = Intent(this@MainActivity, splash::class.java).apply {
+                val i = Intent(this@MainActivity, splashQuiz::class.java).apply {
                     putExtra("carga",0)
                 }
                 startActivity(i)
 
             }
 
+            class splash : MainClass() {
+
+                val question: MutableList<QuestionModel> = mutableListOf()
+
+                private val multiplePermissionId = 14
+                private val multiplePermissionNameList = if (Build.VERSION.SDK_INT >= 33){
+                    arrayListOf(
+                        android.Manifest.permission.READ_MEDIA_IMAGES,
+                        android.Manifest.permission.READ_MEDIA_VIDEO
+                    )
+                } else {
+                    arrayListOf(
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                }
+
+                override fun onCreate(savedInstanceState: Bundle?) {
+
+
+                    super.onCreate(savedInstanceState)
+
+                    setContentView(R.layout.activity_splash)
+
+                    val handler = Handler(Looper.getMainLooper())
+                    handler.postDelayed({
+                        // Verificar y solicitar permisos
+                        if (checkPermissions()) {
+                            continueInitialization()
+                        } else {
+                            requestPermissions()
+                        }
+                    }, 3500)
+
+
+
+                }
+
+                private fun checkPermissions(): Boolean {
+                    for (permission in multiplePermissionNameList) {
+                        if (ContextCompat.checkSelfPermission(
+                                this,
+                                permission
+                            ) != PackageManager.PERMISSION_GRANTED
+                        ) {
+                            return false
+                        }
+                    }
+                    return true
+                }
+                private fun requestPermissions() {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        multiplePermissionNameList.toTypedArray(),
+                        multiplePermissionId
+                    )
+                }
+                override fun onRequestPermissionsResult(
+                    requestCode: Int,
+                    permissions: Array<out String>,
+                    grantResults: IntArray
+                ) {
+                    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+                    if (requestCode == multiplePermissionId) {
+                        if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                            continueInitialization()
+                        } else {
+                            // Permiso(s) denegado(s), manejar en consecuencia
+                        }
+                    }
+                }
+                private fun continueInitialization() {
+                    lifecycleScope.launch(Dispatchers.IO) {
+                        val userProfile = getUserProfile()
+                        if (userProfile.name.isNotEmpty() && userProfile.email.isNotEmpty()) {
+                            // Si el usuario ya se ha logueado antes:
+
+                            startActivity(Intent(this@splash, MainActivity::class.java))
+                            finish()
+
+
+                        } else {
+                            withContext(Dispatchers.Main) {
+
+                                val i = Intent(this@splash, QuizActivity::class.java)
+                                i.putParcelableArrayListExtra("list", ArrayList(question))
+                                startActivity(i)
+                                finish()
+
+                            }
+                        }
+                    }
+                }
+            }
+
             artesBtn.setOnClickListener{
-                val i = Intent(this@MainActivity, splash::class.java).apply {
+                val i = Intent(this@MainActivity, splashQuiz::class.java).apply {
                     putExtra("carga",1)
                 }
                 startActivity(i)
             }
     
             cineBtn.setOnClickListener{
-                val i = Intent(this@MainActivity, splash::class.java).apply {
+                val i = Intent(this@MainActivity, splashQuiz::class.java).apply {
                     putExtra("carga",2)
                 }
                 startActivity(i)
             }
 
             deportesBtn.setOnClickListener{
-                val i = Intent(this@MainActivity, splash::class.java).apply {
+                val i = Intent(this@MainActivity, splashQuiz::class.java).apply {
                     putExtra("carga",3)
                 }
                 startActivity(i)
             }
 
             geografiaBtn.setOnClickListener{
-                val i = Intent(this@MainActivity, splash::class.java).apply {
+                val i = Intent(this@MainActivity, splashQuiz::class.java).apply {
                     putExtra("carga",4)
                 }
                 startActivity(i)
             }
 
             cienciaBtn.setOnClickListener{
-                val i = Intent(this@MainActivity, splash::class.java).apply {
+                val i = Intent(this@MainActivity, splashQuiz::class.java).apply {
                     putExtra("carga",5)
                 }
                 startActivity(i)
             }
 
             musicaBtn.setOnClickListener{
-                val i = Intent(this@MainActivity, splash::class.java).apply {
+                val i = Intent(this@MainActivity, splashQuiz::class.java).apply {
                     putExtra("carga",6)
                 }
                 startActivity(i)
